9a4e7a557e345acd3d5b1132c1ef680cd1349f63
Samuel Alvarez <abrena2@gmail.com>
2025-09-30 17:32:13 +0200

    Add function to operate numbers

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
+
+let numA = 0;
+let numB = 0;
+let operator = "";
