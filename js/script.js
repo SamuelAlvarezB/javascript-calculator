047e95f2fdba1bd11d7912719011f2ed6b818ee9
Samuel Alvarez <abrena2@gmail.com>
2025-09-30 20:12:57 +0200

    Add calculator operation functionality

--- js/script.js
+++ js/script.js
@@ -17,30 +17,53 @@ function divide(a, b) {
 function operate(a, b, operator) {
     switch (operator) {
         case "+":
-            add(a, b);
-            break;
+            return add(a, b);
         case "-":
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case "*":
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case "/":
-            divide(a, b);
-            break;
+            return divide(a, b);
     }
 }
 
 let numA = 0;
 let numB = 0;
+let result = 0;
 let operator = "";
+let operatorCount = 0;
 
 const screen = document.getElementById("screen");
 const allButtons = document.querySelectorAll("button");
 allButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if (e.target.textContent != "CLR" && e.target.textContent != "=") {
+        console.log(e.target.className);
+        if (e.target.className === "btnDigit") {
             screen.textContent = screen.textContent + e.target.textContent;
+        } else if (e.target.className === "btnOperator") {
+            allButtons.forEach((button) => button.style.backgroundColor = "");
+            e.target.style.backgroundColor = "orange";
+            if (operatorCount === 0) {
+                numA = Number.parseFloat(screen.textContent);
+                operator = e.target.textContent;
+            } else {
+                numB = Number.parseInt(screen.textContent);
+                result = operate(numA, numB, operator);
+                numA = result;
+                console.log("Resultado: " + result);
+            }
+            console.log("numA: " + numA + " | numB: " + numB);
+            screen.textContent = "";
+            operatorCount++;
+            operator = e.target.textContent;
+        } else if (e.target.className === "btnEqual") {
+            numB = Number.parseInt(screen.textContent);
+            result = operate(numA, numB, operator);
+            screen.textContent = result;
+            numA = screen.textContent;
+            numB = 0;
+            operatorCount = 0;
+            allButtons.forEach((button) => button.style.backgroundColor = "");
         }
     })
 })
