984b77f2cb5f02dce6bf5c5773a9ad2c48b276ed
Samuel Alvarez <abrena2@gmail.com>
2025-09-30 19:08:01 +0200

    Add button functionality

--- js/script.js
+++ js/script.js
@@ -34,3 +34,13 @@ function operate(a, b, operator) {
 let numA = 0;
 let numB = 0;
 let operator = "";
+
+const screen = document.getElementById("screen");
+const allButtons = document.querySelectorAll("button");
+allButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.textContent != "CLR" && e.target.textContent != "=") {
+            screen.textContent = screen.textContent + e.target.textContent;
+        }
+    })
+})
